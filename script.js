db165c54aaf0eb0b54cdc332f6b1e114ae1d4f48
sarrafdivyanshu-spec <sarrafdivyanshu@gmail.com>
2026-01-22 23:21:55 +0530

    Update script.js 2

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 window.addEventListener("scroll", () => {
-    const nav = document.querySelector(".navbar");
-    nav.style.boxShadow = window.scrollY > 20
-        ? "0 8px 30px rgba(0,0,0,0.3)"
-        : "none";
+  const nav = document.querySelector(".navbar");
+  nav.style.boxShadow =
+    window.scrollY > 30 ? "0 10px 30px rgba(0,0,0,0.4)" : "none";
 });
